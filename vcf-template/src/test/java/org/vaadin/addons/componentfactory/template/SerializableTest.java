d629b7bea49fa7667a5ee5bee059bf74438c3ed8
Paola De Bartolo <30666649+paodb@users.noreply.github.com>
2023-06-26 14:20:16 -0300

    Initial commit

---
+++ vcf-template/src/test/java/org/vaadin/addons/componentfactory/template/SerializableTest.java
@@ -0,0 +1,30 @@
+package org.vaadin.addons.componentfactory.template;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.testutil.ClassesSerializableTest;
+
+import java.util.stream.Stream;
+
+public class SerializableTest extends ClassesSerializableTest {
+
+    private static final UI FAKE_UI = new UI();
+
+    @Override
+    protected void resetThreadLocals() {
+        super.resetThreadLocals();
+        UI.setCurrent(null);
+    }
+
+    @Override
+    protected void setupThreadLocals() {
+        super.setupThreadLocals();
+        UI.setCurrent(FAKE_UI);
+    }
+
+    @Override
+    protected Stream<String> getExcludedPatterns() {
+        return Stream.concat(super.getExcludedPatterns(), Stream.of(
+                "com\\.vaadin\\..*"
+        ));
+    }
+}
