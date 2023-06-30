cfbb42f822d7add20129b7bf26f958aee38dfbd1
Paola De Bartolo <paola.debartolo@gmail.com>
2023-06-30 10:11:53 -0300

    refactor: add drawingLayerOptions

---
+++ vcf-enhanced-map/src/main/java/org/vaadin/addons/componentfactory/enhancedmap/DrawingLayerOptions.java
@@ -0,0 +1,26 @@
+package org.vaadin.addons.componentfactory.enhancedmap;
+
+import elemental.json.Json;
+import elemental.json.JsonObject;
+
+public class DrawingLayerOptions {
+
+	public String fillColor = "rgba(0,255,0,0.4)";
+	public String strokeColor = "green";
+	public double strokeWidth = 2;
+		
+	public DrawingLayerOptions(String fillColor, String strokeColor, double strokeWidth) {
+		this.fillColor = fillColor;
+		this.strokeColor = strokeColor;
+		this.strokeWidth = strokeWidth;
+	}
+
+	public String toJSON() {
+	    JsonObject js = Json.createObject();
+	    js.put("fillColor", fillColor);
+	    js.put("strokeColor", strokeColor);
+	    js.put("strokeWidth", strokeWidth);
+	    return js.toJson();
+	  }
+	
+}
