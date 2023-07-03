cddda4a62581218b03ee06290445292f57a1e9d4
Martin Lopez <mlopez@flowingcode.com>
2023-07-03 12:56:27 -0300

    feat(demo): add draw only one polygon feature demo

--- vcf-enhanced-map-demo/src/main/java/org/vaadin/addons/componentfactory/demo/EnhancedMapDemoView.java
+++ vcf-enhanced-map-demo/src/main/java/org/vaadin/addons/componentfactory/demo/EnhancedMapDemoView.java
@@ -1,24 +1,28 @@
 package org.vaadin.addons.componentfactory.demo;
 
-import com.vaadin.flow.component.HtmlComponent;
-import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.map.Map;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Route;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Polygon;
 import org.vaadin.addons.componentfactory.enhancedmap.DrawingLayerOptions;
 import org.vaadin.addons.componentfactory.enhancedmap.EnhancedMap;
+
+import com.vaadin.flow.component.HtmlComponent;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.map.Map;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.Position;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
 
 /**
  * View for {@link EnhancedMap} demo.
@@ -30,6 +34,7 @@ import org.vaadin.addons.componentfactory.enhancedmap.EnhancedMap;
 public class EnhancedMapDemoView extends VerticalLayout {
     
     private EnhancedMap map;
+    private boolean onePolygon;
 
     public EnhancedMapDemoView() {
         setSizeFull();
@@ -41,22 +46,11 @@ public class EnhancedMapDemoView extends VerticalLayout {
                 
         // Create a map, center viewport on New York using
         // coordinates in EPSG:3857 projection
-        map = new EnhancedMap();
-        map.setSizeFull();
-        map.setZoom(5);
-        map.setCenter(new com.vaadin.flow.component.map.configuration.Coordinate(1238809.9589261413,6044337.659220713));
-        
-        List<Polygon> polygons = new ArrayList<>();
-        GeometryFactory factory = new GeometryFactory();    
-        Polygon polygon = factory.createPolygon(new Coordinate[]{
-                new Coordinate(1238809.9589261413,6044337.659220713),
-                new Coordinate(838311.4548593741,5478624.1969639165),
-                new Coordinate(3299147.79980723,5377107.95485271),
-                new Coordinate(1238809.9589261413,6044337.659220713),
-                });
-        polygons.add(polygon);
-        map.loadPolygons(polygons);        
-        add(map);
+        map = new EnhancedMap(options);
+        initMap();      
+        Div mapPlaceHolder = new Div(map);
+        mapPlaceHolder.setSizeFull();
+        add(mapPlaceHolder);
         
         Button remove = new Button("Remove polygon");
         remove.setDisableOnClick(true);
@@ -84,8 +78,15 @@ public class EnhancedMapDemoView extends VerticalLayout {
             draw.setEnabled(true);
             remove.setEnabled(false);
         });
+        Checkbox cb = new Checkbox("Draw only one polygon", ev-> {
+        	onePolygon = !onePolygon;
+        	mapPlaceHolder.remove(map);
+        	map = new EnhancedMap(new DrawingLayerOptions("rgba(0,255,0,0.4)","purple", 3, onePolygon));
+        	initMap();
+        	mapPlaceHolder.add(map);
+        });
         
-        add(new HorizontalLayout(draw, remove, removeAll)); 
+        add(new HorizontalLayout(draw, remove, removeAll, cb)); 
         
         map.addNewPolygonListener(ev -> {
         	Polygon newPolygon = ev.getPolygon();
@@ -98,6 +99,8 @@ public class EnhancedMapDemoView extends VerticalLayout {
 			}
         	
         	Notification notification = new Notification();
+        	notification.setDuration(500);
+        	notification.setPosition(Position.BOTTOM_END);
         	
         	Button closeButton = new Button(new Icon(VaadinIcon.CLOSE_SMALL));
         	closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE, ButtonVariant.LUMO_CONTRAST);
@@ -112,4 +115,21 @@ public class EnhancedMapDemoView extends VerticalLayout {
         	notification.open();
         });        
     }
+
+	private void initMap() {
+		map.setSizeFull();
+        map.setZoom(5);
+        map.setCenter(new com.vaadin.flow.component.map.configuration.Coordinate(1238809.9589261413,6044337.659220713));
+        
+        List<Polygon> polygons = new ArrayList<>();
+        GeometryFactory factory = new GeometryFactory();    
+        Polygon polygon = factory.createPolygon(new Coordinate[]{
+                new Coordinate(1238809.9589261413,6044337.659220713),
+                new Coordinate(838311.4548593741,5478624.1969639165),
+                new Coordinate(3299147.79980723,5377107.95485271),
+                new Coordinate(1238809.9589261413,6044337.659220713),
+                });
+        polygons.add(polygon);
+        map.loadPolygons(polygons);
+	}
 }
